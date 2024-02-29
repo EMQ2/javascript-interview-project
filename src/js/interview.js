7c99aed164433f45b03f59cb4c4aa86a1ec2737d
notmeegoreng <notmeegoreng@gmail.com>
2024-02-29 09:11:26 +0800

    implement requested functions

--- src/js/interview.js
+++ src/js/interview.js
@@ -1,16 +1,30 @@
 function sort_array(arr) {
-    // Implement your code here
-    return arr;
+    const n = [...arr]
+    n.sort()
+    return n;
 }
 
 function api_integration() {
-    var definitions = [];
-    // Implement your code here
-    return definitions;
+    const word = 'durian'
+    return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then(resp => resp.json())
+        .then(result => result[0]['meanings']
+            .flatMap(meaning => meaning['definitions'].map(e => e['definition']))
+        )
 }
 
 function rainbow_colours() {
-    var arr = [];
-    // Implement your code here
-    return arr;
+    const hex_code = () => {
+        return Math.floor(Math.random() * 256).toString(16)
+    }
+    return [...'rainbow'].map(c => {
+        return {label: c, colour: `#${hex_code()}${hex_code()}${hex_code()}`}
+    });
 }
+
+function test() {
+    console.log(sort_array(['wdc', 'efcd', '12ews', 'azz', 'z']))
+    api_integration().then(r => console.log(r))
+    console.log(rainbow_colours())
+}
+// test()
